0107a41cb816f51e5ad7d60da5ffb0820080fe5c
Carole <carole.velarde@orange.fr>
2017-03-20 22:29:42 +0100

    exercice4 terminé

---
+++ exercice4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>loop4</title>
+</head>
+
+<body>
+
+<?php 
+
+$number = 1;
+
+for($number = 1; $number <= 10; $number = $number + ($number/2)){
+	echo $number." ";
+}
+
+ ?>
+
+</body>
+
+</html>
