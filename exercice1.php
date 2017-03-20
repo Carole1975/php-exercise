f5d27a063d49dc70e03fe4debc39a76f71b01bb1
Carole <carole.velarde@orange.fr>
2017-03-20 21:48:45 +0100

    second exercice terminé

--- exercice1.php
+++ exercice1.php
@@ -9,10 +9,14 @@
 
 <?php
 	
+	$i = 0;
+	
 	for ($i = 0; $i <= 10; $i ++){
 		echo $i;
 	}
 ?>
 
 </body>
+
 </html>
+	
---
+++ exercice2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Loop</title>
+</head>
+
+<body>
+
+<?php 
+
+$number = 0;
+$number1 = 28;
+
+for ($number = 0; $number <= 20; $number ++){
+	echo $number * $number1." ";
+}
+
+ ?>
+</body>
+
+</html>
