c4ae9b46386b213aa35153f9d022b7031af2a340
Carole <carole.velarde@orange.fr>
2017-03-21 16:53:54 +0100

    exercice 8 ok

---
+++ array/exo8.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>array8</title>
+</head>
+
+<body>
+
+<?php
+
+$mois = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+
+foreach ($mois as $month){
+
+echo $month. " ";
+
+}
+
+?>
+
+</body>
+
+</html>
