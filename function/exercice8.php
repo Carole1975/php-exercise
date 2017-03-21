64e19738130a53485e1ceaab2606585fcf8c355e
Carole <carole.velarde@orange.fr>
2017-03-21 15:19:06 +0100

    exercice 8 terminé

---
+++ function/exercice8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>function8</title>
+</head>
+
+<body>
+
+<?php  
+
+function nbr($nb1 = 3, $nb2 = 4, $nb3 = 2){
+	return $nb1 + $nb2 + $nb3;
+}
+
+echo nbr(1, 2, 3);
+
+?>
+
+</body>
+
+</html>
