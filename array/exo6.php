e8142e86d301514f77004865b194f1003f87ec3a
Carole <carole.velarde@orange.fr>
2017-03-21 16:26:32 +0100

    exercice 6 ok

---
+++ array/exo6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>array6</title>
+</head>
+
+<body>
+
+<?php  
+
+$hautDeFrance = array("59" => "Nord", "62" => "Pas de Calais", "80" => "Somme", "60" => "oise", "02" => "Aisne");
+
+echo $hautDeFrance[59];
+
+?>
+
+</body>
+
+</html>
