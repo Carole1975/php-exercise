09713c3e8fb64ce0883d4ddcb9d97b7dc728c596
Carole <carole.velarde@orange.fr>
2017-03-22 15:11:49 +0100

    exercice 5 terminé

---
+++ formulaire/form5.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>formulaire5</title>
+</head>
+
+<body>
+
+
+	<form action = "/user.php" method = "POST"  >
+
+	    <select name ="civilité">
+	      <option value = "Monsieur">Monsieur</option>
+	      <option value = "Madame">Madame</option>
+	      <option value = "Mademoiselle">Mademoiselle</option>
+	    </select>
+
+     
+		<label for = "Name">Nom</label>
+		<input type = "text" name ="Name"/>
+
+		<label for = "FirstName">Prénom</label>
+		<input type = "text" name = "firstName"/>
+
+		<input type="submit" value="envoyer">
+
+	</form>
+
+	<?php
+	echo $_POST["civilité"]." ";  
+    echo $_POST["Name"]." ";
+    echo $_POST["firstName"];
+	?>
+
+
+</body>
+
+</html>
