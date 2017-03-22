7fffd819d2a479fa00f2ac04586618458fa2ae39
Carole <carole.velarde@orange.fr>
2017-03-22 11:13:03 +0100

    exercice 4 terminé

---
+++ url3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>url3</title>
+</head>
+
+<body>
+
+<?php  
+
+echo $_GET["langage"]." ";
+echo $_GET["serveur"];
+
+?>
+
+</body>
+
+</html>
