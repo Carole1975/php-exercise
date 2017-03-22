40729254c71d752133168fd2f0761e4c032f9c77
Carole <carole.velarde@orange.fr>
2017-03-22 11:22:20 +0100

    exercices 5 et 6 terminés

---
+++ url4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>url5</title>
+</head>
+
+<body>
+
+<?php  
+
+echo $_GET["semaine"];
+
+
+?>
+
+</body>
+
+</html>
---
+++ url5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>url6</title>
+</head>
+
+<body>
+
+<?php  
+
+echo $_GET["batiment"]." ";
+echo $_GET["salle"];
+
+
+?>
+
+</body>
+
+</html>
