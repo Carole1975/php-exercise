d35659d1017f5ce0ff974c12632ada099bd0ed21
Carole <carole.velarde@orange.fr>
2017-03-22 11:04:49 +0100

    exercice 3 terminé

---
+++ url2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<title>url3</title>
+</head>
+
+<body>
+
+<?php  
+
+echo $_GET["dateDebut"]." ";
+echo $_GET["dateFin"];
+
+?>
+
+</body>
+
+</html>
