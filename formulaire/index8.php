ce17c192a3920673ce64810f4cb9d7bf328f0eef
Carole <carole.velarde@orange.fr>
2017-03-22 16:54:03 +0100

    exercice 8 terminé

--- formulaire/index8.php
+++ formulaire/index8.php
@@ -9,31 +9,54 @@
 <body>
 
 
-	<form action = "/index.php" method = "POST">
+	<?php
 
-	    <select name ="civilité">
-	      <option value = "Monsieur">Monsieur</option>
-	      <option value = "Madame">Madame</option>
-	      <option value = "Mademoiselle">Mademoiselle</option>
-	    </select>
+	if (!$_POST["civilité"]||! $_POST["Name"]||! $_POST["firstName"]){
 
-     
+		echo'<form action = "index.php" method = "POST">
+
+		<select name ="civilité">
+			<option value = "Monsieur">Monsieur</option>
+			<option value = "Madame">Madame</option>
+			<option value = "Mademoiselle">Mademoiselle</option>
+		</select>
+
+
 		<label for = "Name">Nom</label>
 		<input type = "text" name ="Name"/>
 
 		<label for = "FirstName">Prénom</label>
 		<input type = "text" name = "firstName"/>
 
+		<label for = "Fichier">Fichier</label>
+		<input type="file" name="file">
+
 		<input type="submit" value="envoyer">
 
-	</form>
+	</form>';
 
-	<?php
+
+}
+
+else{
+
 	echo $_POST["civilité"]." ";  
-    echo $_POST["Name"]." ";
-    echo $_POST["firstName"];
-	?>
+	echo $_POST["Name"]." ";
+	echo $_POST["firstName"]." ";
+	echo $_POST["file"]." ";
+	if(explode(".", $_POST["file"])[1] ==="pdf"){
 
+		echo "Ceci est un pdf";
+	}
+
+	else{
+
+		echo "Ceci n'est pas un pdf";
+	}
+
+}
+
+?>
 
 </body>
 
