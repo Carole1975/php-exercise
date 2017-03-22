0c0e6642313d87de29755fe3c430bea92fa6c150
Carole <carole.velarde@orange.fr>
2017-03-22 14:26:31 +0100

    exercice 2 formulaire ok

---
+++ formulaire/form2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>formulaire2</title>
+</head>
+
+<body>
+
+
+	<form action = "/user.php" method = "POST"  >
+
+		<label for = "Name">Nom</label>
+		<input type = "text" />
+
+		<label for = "Firstname">Prénom</label>
+		<input type = "text" "/>
+
+		<input type="submit" value="envoyer">
+
+	</form>
+
+	<?php  
+
+	?>
+
+
+</body>
+
+</html>
