6d8d0ce4cda2e4279523fd91b9158e228fb07163
Carole <carole.velarde@orange.fr>
2017-03-25 00:56:06 +0100

    exercice 2 ok

---
+++ date/exo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Date2</title>
+</head>
+
+<body>
+
+	<?php
+
+	$raw = "25. 03. 2017";
+	$start = DateTime::createFromFormat("d. m. Y", $raw);
+
+	echo "Date: " . $start->format("d-m-Y");
+	
+	?>
+
+</body>
+
+</html>
