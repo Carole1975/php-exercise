34acdd2eb1cab81227a5d5d7427c4b1d2039e4c4
Carole <carole.velarde@orange.fr>
2017-03-26 03:30:52 +0200

    exercice 2 terminé

--- globalVariable2/index.php
+++ globalVariable2/index.php
@@ -3,21 +3,23 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
+	<meta charset="utf-8">
 	<title>global variable2</title>
-	 <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
 
 	<a href=index2.php>Retour</a>
 
-<?php
-session_start();
-$_SESSION["prenom"]="Carole";
-$_SESSION["nom"]="VELARDE";
-$_SESSION["age"]=41;
-?>
+	<p>
+		<?php
+		session_start();
+		$_SESSION["prenom"]="Carole";
+		$_SESSION["nom"]="VELARDE";
+		$_SESSION["age"]=41;
+		?>
+	</p>
 
 </body>
 
--- globalVariable2/index2.php
+++ globalVariable2/index2.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
+	<meta charset="utf-8">
 	<title>Retour</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-<?php  
 
-session_start();
+	<p>
+		<?php  
+		session_start();
 
-echo $_SESSION["nom"]." ";
-echo $_SESSION["prenom"]." ";
-echo $_SESSION["age"]." ";
-?>
+		echo $_SESSION["nom"]." ";
+		echo $_SESSION["prenom"]." ";
+		echo $_SESSION["age"]." ";
+		?>
+	</p>
 
 </body>
 </html>
